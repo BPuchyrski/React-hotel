8cea13c56987681e8675529f65c198a02f3417ed
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-05-06 17:52:39 +0200

    Update Banner.jsx

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import css from "./Banner.module.css";
 const Banner = () => {
   return (
     <div className={css.mainImg}>
-      <h1 className={css.bannerTitle}>B-Balic</h1>
+      <h1 className={css.bannerTitle}>B-Baltic</h1>
       <p className={css.bannerText}>Domki i noclegi w Pobierowie</p>
       <div className={css.lineShort}></div>
       <div className={css.line}></div>
