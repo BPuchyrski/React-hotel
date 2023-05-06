2424fcaea0b017ea49335753722e2ea11d117d65
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-05-06 15:25:10 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/React-hotel/",
+  base: "/React-hotel/",
   plugins: [react()],
+  server: {
+    mimeTypes: {
+      js: "application/javascript",
+      jsx: "text/jsx",
+    },
+  },
 });
