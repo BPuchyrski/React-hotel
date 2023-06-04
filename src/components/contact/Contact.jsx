f54cd1c0cd7ceadbe96b233c793d00bdfd407e3f
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-06-04 13:20:39 +0200

    u

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,24 +5,24 @@ const Contact = () => {
   return (
     <div className={css.overlay}>
       <h2 className={css.contactTitle}>Znajdziesz nas 5 minut od plaży !</h2>
-
-      <p></p>
-      <div>
+      <div className={css.contactInfo}>
         <Map height={300} defaultCenter={[54.06506, 14.95086]} defaultZoom={16}>
           <ZoomControl />
           <Marker width={50} anchor={[54.06506, 14.95086]} />
         </Map>
+        <div className={css.contactInfoBg}>
+          <h3 className={css.contactInfoTitle}>
+            Zapraszam do kontaktu telefonicznego oraz mailowego
+          </h3>
+          <ul className={css.contactList}>
+            <li className={css.contactItem}>BB-ALTIC</li>
+            <li className={css.contactItem}>72-346 Pobierowo</li>
+            <li className={css.contactItem}>ul. Grunwaldzka 118</li>
+            <li className={css.contactItem}>608-800-751</li>
+            <li className={css.contactItem}>kontakt@bb-altic.pl</li>
+          </ul>
+        </div>
       </div>
-      <h3 className={css.contact}>
-        Zapraszam do kontaktu telefonicznego oraz mailowego
-      </h3>
-      <ul className={css.contactList}>
-        <li className={css.contactItem}>BB-ALTIC</li>
-        <li className={css.contactItem}>72-346 Pobierowo</li>
-        <li className={css.contactItem}>ul. Grunwaldzka 118</li>
-        <li className={css.contactItem}>608-800-751</li>
-        <li className={css.contactItem}>kontakt@bb-altic.pl</li>
-      </ul>
     </div>
   );
 };
