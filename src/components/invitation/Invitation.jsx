fcb40069c3d0a039a1d52bdd32e3fce0cf5e471e
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-09-04 18:38:55 +0200

    u

--- src/components/invitation/Invitation.jsx
+++ src/components/invitation/Invitation.jsx
@@ -19,7 +19,7 @@ const Invitation = () => {
         <li className={css.item}>
           <img className={css.icon} src={shell} alt="shell" />
           <h3>WYPOCZYNEK TUŻ PRZY PLAŻY</h3>
-          <p>
+          <p className={css.itemText}>
             Nadmorski klimat, piękna plaża wspaniała atmosfera i ośrodek
             położony 150 m od morza w sosnowym lesie
           </p>
@@ -27,7 +27,7 @@ const Invitation = () => {
         <li className={css.itemPurple}>
           <img className={css.icon} src={checklist} alt="checklist" />
           <h3>JASNE WARUNKI POBYTU</h3>
-          <p>
+          <p className={css.itemText}>
             Zadnych ukrytych kosztów media wliczone w cene pobytu . Parking
             płatny 30 zł za tydzień Opłata klimatyczna pobierana wg stawek
             gminy. Nic Cie na miejscu nie zaskoczy
@@ -36,15 +36,16 @@ const Invitation = () => {
         <li className={css.item}>
           <img className={css.icon} src={map} alt="map" />
           <h3>WSZĘDZIE BLISKO</h3>
-          <p>
+          <p className={css.itemText}>
             150m od plaży, 100m do najbliższej jadłodajni, do dworca 1000m a
-            najbliższy sklep zaraz obok
+            najbliższy sklep zaraz obok. Wszystko co potrzebne jest w zasięgu
+            ręki !
           </p>
         </li>
         <li className={css.itemPurple}>
           <img className={css.icon} src={calendar} alt="calendar" />
           <h3>ELASTYCZNE TERMINY</h3>
-          <p>
+          <p className={css.itemText}>
             Czy planujesz tygdniowy urlop, czy tylko weekendowy wypad u nas
             znajdziesz nocleg. Zadzwoń i zapytaj o wolne terminy.
           </p>
