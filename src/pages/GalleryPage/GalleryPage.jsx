ebca83a1cf8392cd6369377acbe17772ec7968d6
Bartosz Puchyrski <bartosz.puchyrski@gmail.com>
2023-09-15 08:55:08 +0200

    u

--- src/pages/GalleryPage/GalleryPage.jsx
+++ src/pages/GalleryPage/GalleryPage.jsx
@@ -3,127 +3,174 @@ import { useEffect } from "react";
 
 import RoomInfo from "../../components/roomInfo/RoomInfo";
 
-import apartament4os1 from "../../assets/apartament4Os/FB_IMG_1654753644150-min.jpg";
-import apartament4os2 from "../../assets/apartament4Os/IMG_20220503_130956-min.jpg";
-import apartament4os3 from "../../assets/apartament4Os/IMG_20230415_164359-min.jpg";
-import apartament4os4 from "../../assets/apartament4Os/IMG_20230415_164353-min.jpg";
-import apartament4os5 from "../../assets/apartament4Os/IMG_20230415_164453-min.jpg";
-import apartament4os6 from "../../assets/apartament4Os/IMG_20230415_172455-min.jpg";
-import apartament4os7 from "../../assets/apartament4Os/received_1014553596165650-min.jpeg";
-import apartament4os8 from "../../assets/apartament4Os/received_1403714436757494-min.jpeg";
-import apartament4os9 from "../../assets/apartament4Os/received_334090205236986-min.jpeg";
-import apartament4os10 from "../../assets/apartament4Os/received_593678887928828-min.jpeg";
-import apartament4os11 from "../../assets/apartament4Os/received_658301185423163-min.jpeg";
-import morski1 from "../../assets/morski/IMG_20230415_160847-min.jpg";
-import morski2 from "../../assets/morski/IMG_20230415_160903-min.jpg";
-import morski3 from "../../assets/morski/IMG_20230415_160958-min.jpg";
-import morski4 from "../../assets/morski/IMG_20230415_161045-min.jpg";
-import morski5 from "../../assets/morski/IMG_20230415_161208-min.jpg";
-import morski6 from "../../assets/morski/received_257320568700190-min.jpeg";
-import morski9 from "../../assets/morski/received_3523302587683019-min.jpeg";
-import morski10 from "../../assets/morski/received_679999149508888-min.jpeg";
-import granatowy1 from "../../assets/granatowy/IMG-20230628-WA0005.jpg";
-import granatowy2 from "../../assets/granatowy/received_2664648657194855.jpeg";
-import granatowy3 from "../../assets/granatowy/received_3189140327869487.jpeg";
-import granatowy4 from "../../assets/granatowy/received_3523302587683019.jpeg";
+// import granatowy1 from "../../assets/granatowy/IMG-20230628-WA0005.jpg";
+// import granatowy2 from "../../assets/granatowy/received_2664648657194855.jpeg";
+// import granatowy3 from "../../assets/granatowy/received_3189140327869487.jpeg";
+// import granatowy4 from "../../assets/granatowy/received_3523302587683019.jpeg";
 import zielony1 from "../../assets/zielony/IMG_20230415_161208-min.jpg";
 import zielony2 from "../../assets/zielony/received_1391006654443116-min.jpeg";
-import zielony3 from "../../assets/zielony/received_1788703867920111-min.jpeg";
-import zielony4 from "../../assets/zielony/received_259750218468311-min.jpeg";
-import zielony5 from "../../assets/zielony/received_660708191176972-min.jpeg";
-import zielony6 from "../../assets/zielony/received_687727045124822-min.jpeg";
+// import zielony3 from "../../assets/zielony/received_1788703867920111-min.jpeg";
+// import zielony4 from "../../assets/zielony/received_259750218468311-min.jpeg";
+// import zielony5 from "../../assets/zielony/received_660708191176972-min.jpeg";
+// import zielony6 from "../../assets/zielony/received_687727045124822-min.jpeg";
+import polnocny from "../../assets/polnocny/pol.jpg";
+import polnocny1 from "../../assets/polnocny/pol1.jpg";
+import polnocny2 from "../../assets/polnocny/pol2.jpg";
+import polnocny3 from "../../assets/polnocny/pol3.jpg";
+import polnocny4 from "../../assets/polnocny/pol4.jpg";
+import polnocny5 from "../../assets/polnocny/pol5.jpeg";
+import polnocny6 from "../../assets/polnocny/pol6.jpeg";
+import polnocny7 from "../../assets/polnocny/pol7.jpeg";
+import polnocny8 from "../../assets/polnocny/pol8.jpeg";
+import polnocny9 from "../../assets/polnocny/pol9.jpeg";
+import polnocny10 from "../../assets/polnocny/pol10.jpeg";
+import poludniowy from "../../assets/poludniowy/polu.jpg";
+import poludniowy1 from "../../assets/poludniowy/polu1.jpg";
+import poludniowy2 from "../../assets/poludniowy/polu2.jpg";
+import poludniowy3 from "../../assets/poludniowy/polu3.jpg";
+import poludniowy4 from "../../assets/poludniowy/polu4.jpg";
+import poludniowy5 from "../../assets/poludniowy/polu5.jpg";
+import poludniowy6 from "../../assets/poludniowy/polu6.jpg";
+import poludniowy7 from "../../assets/poludniowy/polu7.jpg";
+import poludniowy8 from "../../assets/poludniowy/polu8.jpeg";
+import poludniowy9 from "../../assets/poludniowy/polu9.jpeg";
+import poludniowy10 from "../../assets/poludniowy/polu10.jpeg";
+import poludniowy11 from "../../assets/poludniowy/polu11.jpeg";
+import poludniowy12 from "../../assets/poludniowy/polu12.jpeg";
+import poludniowy13 from "../../assets/poludniowy/polu13.jpeg";
+import zachodni from "../../assets/zachodni/zach.jpeg";
+import zachodni1 from "../../assets/zachodni/zach1.jpg";
+import zachodni2 from "../../assets/zachodni/zach2.jpeg";
+import zachodni3 from "../../assets/zachodni/zach3.jpg";
+import zachodni4 from "../../assets/zachodni/zach4.jpeg";
+import zachodni5 from "../../assets/zachodni/zach5.jpeg";
+import zachodni6 from "../../assets/zachodni/zach6.jpeg";
+import wschodni from "../../assets/wschodni/ws.jpg";
+import wschodni1 from "../../assets/wschodni/ws1.jpg";
+import wschodni2 from "../../assets/wschodni/ws2.jpg";
+import wschodni3 from "../../assets/wschodni/ws3.jpg";
+import wschodni4 from "../../assets/wschodni/ws4.jpeg";
+import wschodni5 from "../../assets/wschodni/ws5.jpeg";
+import wschodni6 from "../../assets/wschodni/ws6.jpeg";
+import wschodni7 from "../../assets/wschodni/ws7.jpeg";
+import wschodni8 from "../../assets/wschodni/ws8.jpeg";
+import wschodni9 from "../../assets/wschodni/ws9.jpeg";
+import morski from "../../assets/morski/mor.jpg";
+import morski1 from "../../assets/morski/mor1.jpg";
+import morski2 from "../../assets/morski/mor2.jpg";
+import morski3 from "../../assets/morski/mor3.jpg";
+import morski4 from "../../assets/morski/mor4.jpg";
+import morski5 from "../../assets/morski/mor5.jpg";
+import morski6 from "../../assets/morski/mor6.jpeg";
+import morski7 from "../../assets/morski/mor7.jpeg";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
 const rooms = [
   {
-    name: "Studio Wschodnie 5/6-os",
-    photo: `${apartament4os1}`,
-    text: "Komfortowy, nowo oddany domek/apartament 5-6-7 osobowy z kominkiem i ogrzewaniem podłogowym. Taras z meblami i grillem, łazienka, pokój dzienny z narożnikiem (może służyć jako dodatkowe spanie ) stolikiem kawowym i TV, kuchnia w pełnym wyposażeniu, na górze sypialnia 2 osobowa i 3 osobowa, w każdy pokój wyposażony w szafę , lampkę nocną, obok sypialni WC . ",
+    name: "APARTAMENT/DOMEK PÓŁNOCNY",
+    photo: `${polnocny3}`,
+    text: `Komfortowy, domek/apartament 5-6-7 osobowy z kominkiem i
+    ogrzewaniem podłogowym. Przed wejsciem zadaszony taras z meblami i grillem, łazienka, pokój  
+    dzienny z narożnikiem (może służyć jako dodatkowe spanie ) stolikiem
+    kawowym i TV, aneks kuchenny w pełnym wyposażeniu, ekspres do kawy, kuchenka mikrofalowa płyta indukcyjna, naczynia.
+    Na górze sypialnia 2 i 3 osobowa, każdy pokój wyposażony w szafę , lampkę nocną, stolik nocny obok sypialni WC . `,
     gallery: [
-      `${apartament4os1}`,
-      `${apartament4os2}`,
-      `${apartament4os3}`,
-      `${apartament4os4}`,
-      `${apartament4os5}`,
-      `${apartament4os6}`,
-      `${apartament4os7}`,
-      `${apartament4os8}`,
-      `${apartament4os9}`,
-      `${apartament4os10}`,
-      `${apartament4os11}`,
+      `${polnocny}`,
+      `${polnocny1}`,
+      `${polnocny2}`,
+      `${polnocny3}`,
+      `${polnocny4}`,
+      `${polnocny5}`,
+      `${polnocny6}`,
+      `${polnocny7}`,
+      `${polnocny8}`,
+      `${polnocny9}`,
+      `${polnocny10}`,
     ],
   },
   {
-    name: "Pokój 2-os morski",
-    photo: `${morski1}`,
-    text: "Nowy komfortowy apartament 3-4 osobowy z ogrzewaniem podłogowym, przed wejściem taras z meblami, pokój dzienny z aneksem kuchennym w pełnym wyposażeniu i TV, przytulna sypialnia z 2 łóżkami jednoosobowymi i szafą, łazienka. Jeśli chcesz przyjechać z przyjaciółmi dysponujemy dwoma taki apartamentami obok siebie. Zapraszam :)",
+    name: "APARTAMENT/ DOMEK POŁUDNIOWY",
+    photo: `${poludniowy4}`,
+    text: `Komfortowy, domek/apartament 5-6-7 osobowy z ogrzewaniem podłogowym. Przed wejsciem zadaszony taras z meblami i grillem, łazienka, pokój
+    dzienny z narożnikiem (może służyć jako dodatkowe spanie ) stolikiem kawowym i TV, aneks kuchenny w pełnym wyposażeniu, ekspres do kawy, kuchenka mikrofalowa płyta indukcyjna, naczynia. Na górze sypialnia 2 i 3 osobowa, każdy pokój wyposażony w szafę , lampkę nocną, stolik nocny obok sypialni WC .`,
     gallery: [
+      `${poludniowy}`,
+      `${poludniowy1}`,
+      `${poludniowy2}`,
+      `${poludniowy3}`,
+      `${poludniowy4}`,
+      `${poludniowy5}`,
+      `${poludniowy6}`,
+      `${poludniowy7}`,
+      `${poludniowy8}`,
+      `${poludniowy9}`,
+      `${poludniowy10}`,
+      `${poludniowy11}`,
+      `${poludniowy12}`,
+      `${poludniowy13}`,
+    ],
+  },
+  {
+    name: "APARTAMENT 4 OSOBOWY ZACHODNI",
+    photo: `${zachodni1}`,
+    text: `Komfortowy apartament 3-4 osobowy z ogrzewaniem podłogowym, przed
+    wejściem taras z meblami, pokój dzienny w którym znajduje sie łóżko 2 osobowe. Pokój połączony z aneksem  kuchennym w pełnym
+    wyposażeniu TV, przytulna sypialnia z 2 łóżkami jednoosobowymi, szafą, łazienka.
+    Jeśli chcesz przyjechać z przyjaciółmi dysponujemy
+    dwoma taki apartamentami obok siebie. Zapraszam :)`,
+    gallery: [
+      `${zachodni1}`,
+      `${zachodni2}`,
+      `${zachodni3}`,
+      `${zachodni4}`,
+      `${zachodni5}`,
+      `${zachodni6}`,
+      `${zachodni}`,
+    ],
+  },
+  {
+    name: "APARTAMENT 4 OSOBOWY WSCHODNI",
+    photo: `${wschodni9}`,
+    text: `Komfortowy apartament 3-4 osobowy z ogrzewaniem podłogowym, przed wejściem taras z meblami, pokój dzienny w którym znajduje sie łóżko 2 osobowe. Pokój połączony z aneksem  kuchennym w pełnym wyposażeniu TV, przytulna sypialnia z 2 łóżkami jednoosobowymi, szafą, łazienka. Jeśli chcesz przyjechać z przyjaciółmi dysponujemy
+    dwoma taki apartamentami obok siebie. Zapraszam :)`,
+    gallery: [
+      `${wschodni}`,
+      `${wschodni1}`,
+      `${wschodni2}`,
+      `${wschodni3}`,
+      `${wschodni4}`,
+      `${wschodni5}`,
+      `${wschodni6}`,
+      `${wschodni7}`,
+      `${wschodni8}`,
+      `${wschodni9}`,
+    ],
+  },
+  {
+    name: "POKÓJ MORSKI 2 OSOBOWY",
+    photo: `${morski}`,
+    text: `Pokój  z łazienką łóżkiem 2 osobowym,   TV. W pokoju czajnik, lodówka kącik kuchenny, szafa stól z krzesłami obok pokoju aneks kuchenny w pełnym wyposażeniu.  Przed wejściem taras z meblami  Możliwość skorzystania z altanki i grila Do dyspozycji gości miejsca parkingowe, miejsca do grilowania, plac zabaw dla dzieci, rowery sprzęt plażowy.`,
+    gallery: [
+      `${morski}`,
       `${morski1}`,
       `${morski2}`,
       `${morski3}`,
       `${morski4}`,
       `${morski5}`,
       `${morski6}`,
-      `${morski9}`,
-      `${morski10}`,
-    ],
-  },
-  {
-    name: "Pokój 3/4-os granatowy",
-    photo: `${granatowy1}`,
-    text: "Pokoje z łazienkami granatowy 3-4 osobowy i pokój pomarańczowy 2-3 osobowy, aneks kuchenny, taras",
-    gallery: [
-      `${granatowy1}`,
-      `${granatowy2}`,
-      `${granatowy3}`,
-      `${granatowy4}`,
-    ],
-  },
-  {
-    name: "Pokój 3-os zielony",
-    photo: `${zielony5}`,
-    text: "Nowy komfortowy apartament 3-4 osobowy z ogrzewaniem podłogowym, przed wejściem taras z meblami, pokój dzienny z aneksem kuchennym w pełnym wyposażeniu i TV, przytulna sypialnia z 2 łóżkami jednoosobowymi i szafą, łazienka. Jeśli chcesz przyjechać z przyjaciółmi dysponujemy dwoma taki apartamentami obok siebie. Zapraszam :)",
-    gallery: [
-      `${zielony1}`,
-      `${zielony2}`,
-      `${zielony3}`,
-      `${zielony4}`,
-      `${zielony6}`,
-    ],
-  },
-  {
-    name: "Pokój 2-os pomarańczowy",
-    photo: `${granatowy1}`,
-    text: "Pokoje z łazienkami granatowy 3-4 osobowy i pokój pomarańczowy 2-3 osobowy, aneks kuchenny, taras",
-    gallery: [
-      `${granatowy1}`,
-      `${granatowy2}`,
-      `${granatowy3}`,
-      `${granatowy4}`,
+      `${morski7}`,
     ],
   },
   {
     name: "Studio zachodnie 4-os",
-    photo: `${apartament4os1}`,
+    photo: `${zielony1}`,
     text: `Komfortowy apartament 3-4 osobowy z ogrzewaniem podłogowym, przed
     wejściem taras z meblami, pokój dzienny z aneksem kuchennym w pełnym
     wyposażeniu i TV, przytulna sypialnia z 2 łóżkami jednoosobowymi i
     szafą, łazienka. Jeśli chcesz przyjechać z przyjaciółmi dysponujemy
     dwoma taki apartamentami obok siebie.`,
-    gallery: [
-      `${apartament4os1}`,
-      `${apartament4os2}`,
-      `${apartament4os3}`,
-      `${apartament4os4}`,
-      `${apartament4os5}`,
-      `${apartament4os6}`,
-      `${apartament4os7}`,
-      `${apartament4os8}`,
-      `${apartament4os9}`,
-      `${apartament4os10}`,
-      `${apartament4os11}`,
-    ],
+    gallery: [`${zielony1}`, `${zielony2}`],
   },
 ];
 
